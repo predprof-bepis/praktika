793118ab1ea1e77169d43e3e126fc8fcb5bdc34e
Northkatz <koffichoc@gmail.com>
2026-01-21 21:48:49 +0300

    fix routing problems

--- web/app.py
+++ web/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import routes
+
 
 app = Flask(__name__)
 
-from . import routes 
+routes.add_routes(app)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
