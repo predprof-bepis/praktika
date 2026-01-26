359156ebf55f90df38052436acda875e49c12ea7
ARCTIC_SALAMANDER <ruslanau365@gmail.com>
2026-01-26 20:19:57 +0300

    добавила страницу для скачивания pdf-отчета

--- web/routes.py
+++ web/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template
-from app import app
 
-
-@app.route('/', methods=['GET'])
-def mainPage():
-    return render_template('index.html')
+def add_routes(app):
+    @app.route('/', methods=['GET'])
+    def mainPage():
+        return render_template('index.html')
