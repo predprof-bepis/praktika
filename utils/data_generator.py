d512af1a82735b81e46a0849808a97ded772c994
ARCTIC_SALAMANDER <ruslanau365@gmail.com>
2026-02-07 14:24:40 +0300

    сделала генерацию данных в таблицы programs, applications

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -1,10 +1,32 @@
-from dbtools import DB
 import sys
 import os
+import random
 
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
+
+
+from dbtools import DB
+
+
+def program_data(db):
+    db.run('''
+        INSERT INTO programs(name, budget_seats) 
+        VALUES ("pm", 40)
+    ''')
+    db.run('''
+        INSERT INTO programs(name, budget_seats) 
+        VALUES ("ivt", 50)
+    ''')
+    db.run('''
+        INSERT INTO programs(name, budget_seats) 
+        VALUES ("itss", 30)
+    ''')
+    db.run('''
+        INSERT INTO programs(name, budget_seats) 
+        VALUES ("ib", 20)
+    ''')
 
 
 def applicants_data(db):
@@ -24,11 +46,42 @@ def applicants_data(db):
     ''')
 
 
+def applications_data(db):
+    for i in range(1, 101):
+        program_id = random.randint(1, 4)
+        many_programs = random.randint(0, 1)
+        consent = random.randint(0, 1)
+        helper = db.run('''
+            INSERT INTO applications (applicant_id, program_id, date, priority, consent) 
+            VALUES (?, ?, "2026-08-04", ?, ?)
+        ''', (i, program_id, program_id, consent)) # пока что дата только 04.08
+
+        if many_programs == 1:
+            program_count = random.randint(1, 3) # на сколько программ подаемся
+
+            already_applied = [program_id,]
+            priority = program_id
+            
+            for i in range(program_count):
+                program_id = 1
+                while program_id in already_applied:
+                    program_id += 1
+
+                helper = db.run('''
+                    INSERT INTO applications (applicant_id, program_id, date, priority, consent)
+                    VALUES (?, ?, "2026-08-04", ?, ?)
+                ''', (i, program_id, priority, consent))
+
+                already_applied.append(program_id)
+
+
 db = DB()
 db.create_tables()
 
-db.run('''
-    DELETE FROM applicants
-''')
+# чистка
+db.run('''DELETE FROM applicants''')
+db.run('DELETE FROM sqlite_sequence WHERE name = "applicants"')
 
 applicants_data(db)
+program_data(db)
+applications_data(db)
