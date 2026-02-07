35c9eda200fd6b414eaabdae4169ec1fb120db54
ARCTIC_SALAMANDER <ruslanau365@gmail.com>
2026-02-07 21:03:50 +0300

    сделала подсчет проходного балла

--- core/database.py
+++ core/database.py
@@ -14,6 +14,13 @@ class DBManager:
         self.fileName = fileName
         self.db = DB(self.fileName)
 
+        self.places_count = {
+            "pm": 40,
+            "ivt": 50,
+            "itss": 30,
+            "ib": 20
+        }
+
     def db_filter(self, programs: list, date="2026-08-04") -> dict:
         res = dict()
         for program in programs:
@@ -27,4 +34,53 @@ class DBManager:
                     )
                 ''', date, program)
             
+        return res
+    
+    def _compare_applicants(self, applicant1: tuple, applicant2: tuple):
+            if applicant1[1] > applicant2[1]:
+                return 1
+            elif applicant1[1] < applicant2[1]:
+                return -1
+            else:
+                if applicant1[2] < applicant2[2]:
+                    return -1
+                elif applicant1[2] > applicant2[2]:
+                    return 1
+                else:
+                    return 0
+                
+    def count_accepted(self, data: list[tuple[int, int, int]]):
+        count = 0
+        for i in data:
+            if i[1] == 1:
+                count += 1
+
+        return count
+
+    def count_pass_score(self, programs: list, data: dict):
+        '''Подсчет проходного балла для каждой программы. data ужен обязательно с данными
+        из столбцов consent и total_score'''
+        from functools import cmp_to_key
+
+
+        res = dict()
+        for program in programs:
+            if program not in data.keys():
+                continue
+
+            program_data = data[program]
+            program_data = sorted(program_data, key=cmp_to_key(self._compare_applicants), reverse=True)
+
+            accepted = self.count_accepted(program_data)
+            if accepted >= self.places_count[program]:
+                res[program] = 0 # все места уже заняты
+                continue
+            else:
+                try: # подают больше заявок, чем мест
+                    score = program_data[self.places_count[program] - 1][2]
+                except IndexError:
+                    score = program_data[-1][2]
+
+            res[program] = score
+
         return res
