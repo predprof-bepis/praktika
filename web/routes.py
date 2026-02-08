8175bff325132d250777e719d68697e13428b02c
ARCTIC_SALAMANDER <ruslanau365@gmail.com>
2026-02-08 20:06:53 +0300

    сделала страницу для загрузки бд на сайт, создала файл для логики сайта

--- web/routes.py
+++ web/routes.py
@@ -8,3 +8,7 @@ def add_routes(app):
     @app.route('/download-pdf', methods=['GET'])
     def downloadPdfPage():
         return render_template('download-pdf.html')
+    
+    @app.route('/upload-db', methods=['GET', 'POST'])
+    def uploadDbPage():
+        return render_template('upload-db.html')
