5d72e372426cf24af5a2d3c97f0e70ca7891a953
Northkatz <koffichoc@gmail.com>
2026-02-09 15:13:49 +0300

    функционал пдф коммит 2

--- web/routes.py
+++ web/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, request
+from flask import render_template, request, send_file
 import logic
+import pdfgenerator
 
 
 def add_routes(app):
@@ -7,17 +8,43 @@ def add_routes(app):
     def mainPage():
         if request.method == "POST":
             selected_date = request.form.get('date')
+            selected_program = request.form.get('program')
+
             scores = logic.get_scores(selected_date)
+            places = logic.get_places_counts(selected_date)
+            data = logic.get_data(selected_date, selected_program)
         else:
+            selected_date="Нет дат"
+            selected_program = "pm"
             scores = ""
+            places = {
+                "pm": "Нет данных",
+                "ivt": "Нет данных",
+                "itss": "Нет данных",
+                "ib": "Нет данных"
+            }
+            data = {"pm": ("Нет данных", "Нет данных", "Нет данных")}
 
         dates = logic.get_dates()
 
-        return render_template('index.html', dates=dates, scores=scores)
+        return render_template('index.html', 
+                                dates=dates, 
+                                scores=scores, 
+                                date=selected_date, 
+                                program=selected_program,
+                                places=places,
+                                data=data)
     
-    @app.route('/download-pdf', methods=['GET'])
+    @app.route('/download-pdf', methods=['GET', 'POST'])
     def downloadPdfPage():
-        return render_template('download-pdf.html')
+        match request.method:
+            case "POST":
+                print(request.form.get("date"))
+                a = pdfgenerator.PDFGenerator(logic.db_manager.db, logic.db_manager)
+                a.generate_pdf("resultador.pdf", request.form.get("date"))
+                return send_file('resultador.pdf')
+            case "GET":
+                return render_template('download-pdf.html', dates=logic.get_dates())
     
     @app.route('/upload-db', methods=['GET', 'POST'])
     def uploadDbPage():
